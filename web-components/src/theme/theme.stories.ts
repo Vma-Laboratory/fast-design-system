f7975a4c157dd8fb983c1f436aef59de0dadc71f
Vincent Margan <margan.vincent@gmail.com>
2023-07-18 17:56:30 +0200

    WIP

--- web-components/src/theme/theme.stories.ts
+++ web-components/src/theme/theme.stories.ts
@@ -1,17 +1,20 @@
 import * as tokens from "../../../tokens/src";
 import { html } from "@microsoft/fast-element";
 import { renderComponent } from "../helpers.storybook";
+import { Meta } from "@storybook/web-components";
+
 export default {
   title: "Theme/Tokens",
-};
+  parameters: { options: { showPanel: false } },
+} as Meta;
 
 export const Tokens = renderComponent(html`
-  <section class="display:flex;flex-direction:column">
+  <section class="display:flex;flex-direction:column;font-size:${tokens.fpsfaFontSizesBody};font-weight:${tokens.fpsfaFontWeightsRoboto5}; font-family:${tokens.fpsfaFontFamiliesRoboto}">
     <h3>Theme Tokens</h3>
     <p>Debug story which uses theme tokens to style the elements below.</p>
     <h4>Color Palette</h4>
     <section
-      style="display:grid;grid-template-columns: repeat(4, 1fr);gap:0.25rem;font-size:${tokens.fpsfaFontSizesBody};font-weight:${tokens.fpsfaFontWeightsRoboto5};background-color:${tokens.fpsfaThemePrimaryFixed}; padding:1px; font-family:${tokens.fpsfaFontFamiliesRoboto}"
+      style="display:grid;grid-template-columns: repeat(4, 1fr);gap:0.25rem;background-color:${tokens.fpsfaThemePrimaryFixed}; padding:1px;margin-bottom:1rem;"
     >
       <div>
         <div
@@ -91,7 +94,6 @@ export const Tokens = renderComponent(html`
         </div>
       </div>
 
-      
       <div>
         <div
           style="height:40px;padding:8px;background-color:${tokens.fpsfaThemeTertiaryContainer};color:${tokens.fpsfaThemeOnTertiaryContainer}"
@@ -186,6 +188,178 @@ export const Tokens = renderComponent(html`
           On Tertiary Fixed Variant
         </div>
       </div>
+      <div><!-- EMPTY --></div>
+
+      <div
+        style="height:70px;padding:8px;background-color:${tokens.fpsfaThemeSurfaceDim};color:${tokens.fpsfaThemeOnSurface}"
+      >
+        Surface Dim
+      </div>
+      <div
+        style="height:70px;padding:8px;background-color:${tokens.fpsfaThemeSurface};color:${tokens.fpsfaThemeOnSurface}"
+      >
+        Surface
+      </div>
+      <div
+        style="height:70px;padding:8px;background-color:${tokens.fpsfaThemeSurfaceBright};color:${tokens.fpsfaThemeOnSurface}"
+      >
+        Surface Bright
+      </div>
+      <div>
+        <div
+          style="height:27px;padding:8px;background-color:${tokens.fpsfaThemeInverseSurface};color:${tokens.fpsfaThemeInverseOnSurface}"
+        >
+          Inverse Surface
+        </div>
+        <div
+          style="height:27px;padding:8px;background-color:${tokens.fpsfaThemeInverseOnSurface};color:${tokens.fpsfaThemeInverseSurface}"
+        >
+          Inverse On Surface
+        </div>
+      </div>
+      <div style="grid-column-start: 1;grid-column-end: 4;display:flex;" >
+        <div
+          style="height:60px;flex:1;padding:8px;background-color:${tokens.fpsfaThemeSurfaceContainerLowest};color:${tokens.fpsfaThemeOnSurface}"
+        >
+          Surf. Container Lowest
+        </div>
+        <div
+          style="height:60px;flex:1;padding:8px;background-color:${tokens.fpsfaThemeSurfaceContainerLow};color:${tokens.fpsfaThemeOnSurface}"
+        >
+          Surf. Container Low
+        </div>
+        <div
+          style="height:60px;flex:1;padding:8px;background-color:${tokens.fpsfaThemeSurfaceContainer};color:${tokens.fpsfaThemeOnSurface}"
+        >
+          Surf. Container
+        </div>
+        <div
+          style="height:60px;flex:1;padding:8px;background-color:${tokens.fpsfaThemeSurfaceContainerHigh};color:${tokens.fpsfaThemeOnSurface}"
+        >
+          Surf. Container High
+        </div>
+        <div
+          style="height:60px;flex:1;padding:8px;background-color:${tokens.fpsfaThemeSurfaceContainerHighest};color:${tokens.fpsfaThemeOnSurface}"
+        >
+          Surf. Container Highest
+        </div>
+      </div>
+      <div
+        style="height:30px;padding:8px;background-color:${tokens.fpsfaThemeInversePrimary};color:${tokens.fpsfaThemeInverseSurface}"
+      >
+        Inverse Primary
+      </div>
+
+      <div style="grid-column-start: 1;grid-column-end: 4;display:flex;" >
+        <div
+            style="height:30px;flex:1;padding:8px;background-color:${tokens.fpsfaThemeOnSurface};color:${tokens.fpsfaThemeSurface}"
+          >
+            On Surface
+        </div>
+        <div
+            style="height:30px;flex:1;padding:8px;background-color:${tokens.fpsfaThemeOnSurfaceVariant};color:${tokens.fpsfaThemeSurface}"
+          >
+            On Surface Var.
+        </div>
+        <div
+            style="height:30px;flex:1;padding:8px;background-color:${tokens.fpsfaThemeOutline};color:${tokens.fpsfaThemeSurface}"
+          >
+            Outline
+        </div>
+        <div
+            style="height:30px;flex:1;padding:8px;background-color:${tokens.fpsfaThemeOutlineVariant};color:${tokens.fpsfaThemeOnSurface}"
+          >
+            Outline Variant
+        </div>
+      </div>
+      <div style="display:flex;gap:1rem;">
+        <div  style="height:30px;flex:1;padding:8px;background-color:${tokens.fpsfaThemeScrim};color:${tokens.fpsfaThemeInverseOnSurface}">
+          Scrim
+        </div>
+        <div  style="height:30px;flex:1;padding:8px;background-color:${tokens.fpsfaThemeShadow};color:${tokens.fpsfaThemeInverseOnSurface}">
+          Shadow
+        </div>
+      </div>
+
+    </section>
+    <section
+      style="display:grid;grid-template-columns: repeat(4, 1fr);gap:0.25rem;"
+    >
+      <div>
+        <div
+          style="height:40px;padding:8px;background-color:${tokens.fpsfaThemeSuccess};color:${tokens.fpsfaThemeOnSuccess}"
+        >
+          Success
+        </div>
+        <div
+          style="height:40px;padding:8px;background-color:${tokens.fpsfaThemeOnSuccess};color:${tokens.fpsfaThemeSuccess}"
+        >
+          On Success
+        </div>
+      </div>
+      <div>
+        <div
+          style="height:40px;padding:8px;background-color:${tokens.fpsfaThemeWarning};color:${tokens.fpsfaThemeOnWarning}"
+        >
+          Warning
+        </div>
+        <div
+          style="height:40px;padding:8px;background-color:${tokens.fpsfaThemeOnWarning};color:${tokens.fpsfaThemeWarning}"
+        >
+          On Warning
+        </div>
+      </div>
+      <div>
+        <div
+          style="height:40px;padding:8px;background-color:${tokens.fpsfaThemeInformation};color:${tokens.fpsfaThemeOnInformations}"
+        >
+          Information
+        </div>
+        <div
+          style="height:40px;padding:8px;background-color:${tokens.fpsfaThemeOnInformations};color:${tokens.fpsfaThemeInformation}"
+        >
+          On Information
+        </div>
+      </div>
+      <div><!-- EMPTY --></div>
+
+      <div>
+        <div
+          style="height:40px;padding:8px;background-color:${tokens.fpsfaThemeSuccessContainer};color:${tokens.fpsfaThemeOnSuccessContainer}"
+        >
+          Success Container
+        </div>
+        <div
+          style="height:40px;padding:8px;background-color:${tokens.fpsfaThemeOnSuccessContainer};color:${tokens.fpsfaThemeSuccessContainer}"
+        >
+          On Success Container
+        </div>
+      </div>
+      <div>
+        <div
+          style="height:40px;padding:8px;background-color:${tokens.fpsfaThemeWarningContainer};color:${tokens.fpsfaThemeOnWarningContainer}"
+        >
+          Warning Container
+        </div>
+        <div
+          style="height:40px;padding:8px;background-color:${tokens.fpsfaThemeOnWarningContainer};color:${tokens.fpsfaThemeWarningContainer}"
+        >
+          On Warning Container
+        </div>
+      </div>
+      <div>
+        <div
+          style="height:40px;padding:8px;background-color:${tokens.fpsfaThemeInformationsContainer};color:${tokens.fpsfaThemeOnInformationsContainer}"
+        >
+          Information Container
+        </div>
+        <div
+          style="height:40px;padding:8px;background-color:${tokens.fpsfaThemeOnInformationsContainer};color:${tokens.fpsfaThemeInformationsContainer}"
+        >
+          On Information Container
+        </div>
+      </div>
+      <div><!-- EMPTY --></div>
 
     </section>
   </section>
